7467faaee9c5285bafdf0dac5fc7b2c8e216feb7
AlirezaIvaz <ivaz1382@gmail.com>
2024-01-27 19:12:58 +0330

    Cleanup codes

--- src/main/kotlin/HomeScreen.kt
+++ src/main/kotlin/HomeScreen.kt
@@ -1,8 +1,6 @@
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.BrightnessAuto
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
